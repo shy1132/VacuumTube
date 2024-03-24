73b935610278ce418fe319fc8a1601d40854f4aa
shy <70299052+whakama@users.noreply.github.com>
2024-03-23 20:07:28 -0500

    the code

---
+++ index.js
@@ -0,0 +1,43 @@
+//initialization
+const electron = require('electron')
+const path = require('path')
+const { app, BrowserWindow } = electron
+
+//code
+const chromeVersion = process.versions.chrome.split('.')[0]
+const userAgent = `Mozilla/5.0 (Linux; Smart TV) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${chromeVersion}.0.0.0 Safari/537.36`
+const runningOnDeck = process.env.SteamOS === '1' && process.env.SteamGamepadUI === '1'
+
+let mainWindow;
+
+async function createWindow() {
+    if (runningOnDeck) app.commandLine.appendSwitch('--no-sandbox') //wont run without this in game mode, wont run on my pc with it
+
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 600,
+        useContentSize: true,
+        skipTaskbar: false,
+        autoHideMenuBar: true,
+        backgroundColor: '#282828',
+        fullscreen: !!runningOnDeck, //if running on steam deck in game mode, start in fullscreen
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: false,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    mainWindow.loadURL('https://www.youtube.com/tv', { userAgent })
+
+    electron.session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
+        if (details.requestHeaders['User-Agent']) details.requestHeaders['User-Agent'] = userAgent
+        callback({ cancel: false, requestHeaders: details.requestHeaders })
+    })
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    })
+}
+
+app.whenReady().then(createWindow)
---
+++ preload.js
@@ -0,0 +1,140 @@
+let gamepadKeyCodeMap = {
+    0: 13, //a -> enter
+    1: 8, //b -> backspace
+    8: 27, //select -> escape
+    9: 13, //start -> enter
+    12: 38, //dpad up -> arrow key up
+    13: 40, //dpad down -> arrow key down
+    14: 37, //dpad left -> arrow key left
+    15: 39 //dpad right -> arrow key right
+}
+
+let pressedButtons = {}
+let keyRepeatInterval = 100;
+let keyRepeatTimeout;
+let keyRepeatDelay = 500;
+
+window.addEventListener('DOMContentLoaded', () => {
+    waitForElement('.html5-video-player', (el) => {
+        el.setPlaybackQualityRange('highres') //allow high res video playback
+
+        function adBlock() {
+            let isAd = el?.classList.contains('ad-interrupting') || el?.classList.contains('ad-showing')
+            if (isAd) {
+                let videoPlayer = document.querySelector('.video-stream')
+                if (!videoPlayer || !videoPlayer.currentTime) return;
+                videoPlayer.currentTime = videoPlayer.duration;
+
+                console.log('ad skipped')
+            }
+        }
+
+        setInterval(adBlock)
+    })
+
+    window.addEventListener('gamepadconnected', (event) => {
+        requestAnimationFrame(() => checkControllerInput(event.gamepad.index))
+    })
+
+    window.addEventListener('gamepaddisconnected', () => {
+        pressedButtons = {}
+    })
+
+    function checkControllerInput(index) {
+        let gamepad = navigator.getGamepads()[index]
+
+        if (gamepad) {
+            for (let i = 0; i < gamepad.buttons.length; i++) {
+                let keyCode = gamepadKeyCodeMap[i]
+                if (!keyCode) continue;
+
+                let button = gamepad.buttons[i]
+                let buttonWasPressed = pressedButtons[i]
+
+                if (button.pressed && !buttonWasPressed) {
+                    pressedButtons[i] = true;
+                    simulateKeyDown(keyCode)
+                    stopKeyRepeat()
+                    keyRepeatTimeout = setTimeout(() => startKeyRepeat(keyCode), keyRepeatDelay)
+                } else if (!button.pressed && buttonWasPressed) {
+                    pressedButtons[i] = false;
+                    simulateKeyUp(keyCode)
+                    stopKeyRepeat()
+                }
+            }
+
+            for (let i = 0; i < gamepad.axes.length; i++) {
+                let axisValue = gamepad.axes[i];
+                let keyCode = null;
+                let axisIndex = i + gamepad.buttons.length; //this is kind of hacky but i dont mind
+
+                let axisWasPressed = pressedButtons[axisIndex];
+            
+                if (i === 0) {
+                    if (axisValue > 0.5) {
+                        keyCode = 39; //right arrow
+                    } else if (axisValue < -0.5) {
+                        keyCode = 37; //left arrow
+                    }
+                } else if (i === 1) {
+                    if (axisValue > 0.5) {
+                        keyCode = 40; //down arrow
+                    } else if (axisValue < -0.5) {
+                        keyCode = 38; //up arrow
+                    }
+                }
+
+                if (keyCode != null) {
+                    if (!axisWasPressed) {
+                        pressedButtons[axisIndex] = true;
+                        simulateKeyDown(keyCode)
+                        stopKeyRepeat()
+                        keyRepeatTimeout = setTimeout(() => startKeyRepeat(keyCode), keyRepeatDelay)
+                    }
+                } else {
+                    if (axisWasPressed) {
+                        pressedButtons[axisIndex] = false;
+                        simulateKeyUp(keyCode)
+                        stopKeyRepeat()
+                    }
+                }
+            }
+        }
+
+        requestAnimationFrame(() => checkControllerInput(index))
+    }
+
+    function simulateKeyDown(keyCode) {
+        let event = new Event('keydown')
+        event.keyCode = keyCode
+        document.dispatchEvent(event)
+    }
+
+    function simulateKeyUp(keyCode) {
+        let event = new Event('keyup')
+        event.keyCode = keyCode
+        document.dispatchEvent(event)
+    }
+
+    function startKeyRepeat(keyCode) {
+        clearInterval(keyRepeatTimeout)
+        clearTimeout(keyRepeatTimeout)
+        keyRepeatTimeout = setInterval(() => simulateKeyDown(keyCode), keyRepeatInterval)
+    }
+
+    function stopKeyRepeat() {
+        clearInterval(keyRepeatTimeout)
+    }
+})
+
+function waitForElement(selector, callback) {
+    function checkElement() {
+        if (document.querySelector(selector)) {
+            callback(document.querySelector(selector))
+        } else {
+            setTimeout(checkElement, 50)
+        }
+    }
+
+    checkElement()
+}
